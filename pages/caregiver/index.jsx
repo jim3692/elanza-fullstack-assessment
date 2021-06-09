c151fd8fe79af7d4f19f47f9d6ad22608d347464
jim3692 <jim3692@gmail.com>
2021-06-09 20:01:00 +0300

    Implement caregiver request details

--- pages/caregiver/index.jsx
+++ pages/caregiver/index.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 
 export default function CaregiverOverviewPage () {
   const [requests, setRequests] = useState([])
+  const [details, setDetails] = useState(null)
 
   useEffect(
     () => axios('/api/caregiver/care-requests')
@@ -12,11 +13,30 @@ export default function CaregiverOverviewPage () {
     , []
   )
 
+  const handleRequestClick = id => axios('/api/caregiver/care-requests/' + id)
+    .then(res => res.data)
+    .then(setDetails)
+    .catch(console.error) // TODO
+
   return (
-    <ul>
-      {requests.map(request => (
-        <div key={request.id}>{request.customerName}</div>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {requests.map(request => (
+          <div key={request.id} onClick={() => handleRequestClick(request.id)}>{request.customerName}</div>
+        ))}
+      </ul>
+      {details && (
+        <div>
+          <label htmlFor='name'>Customer Name</label>
+          <input id='name' type='text' value={details.customerName} disabled />
+          <br />
+          <label htmlFor='request'>Request</label>
+          <input id='request' type='text' value={details.request} disabled />
+          <br />
+          <label htmlFor='date'>Request Date</label>
+          <input id='date' type='date' value={details.date} disabled />
+        </div>
+      )}
+    </div>
   )
 }
--- src/api/caregiver.js
+++ src/api/caregiver.js
@@ -4,9 +4,23 @@ const { careRequests } = require('../../database')
 const router = Router()
 
 router.get('/care-requests', getCareRequests)
+router.get('/care-requests/:id', getCareRequestDetails)
 
 function getCareRequests (req, res) {
   return res.json(careRequests.data)
+}
+
+function getCareRequestDetails (req, res) {
+  const careRequest = careRequests.data.find(request => request.id.toString() === req.params.id.toString())
+
+  if (!careRequest) {
+    return res.status(404)
+      .json({
+        error: `Care request ${req.params.id} not found.`
+      })
+  }
+
+  return res.json(careRequest)
 }
 
 module.exports = router
