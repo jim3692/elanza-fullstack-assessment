318693d8bae6635c831b49477b2c98ad5cd4d68a
jim3692 <jim3692@gmail.com>
2021-06-09 18:33:34 +0300

    Linting with standard

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 // this is the in-memory database ;)
-let counter = 0;
+let counter = 0
 
 app.get('/api/data', function (req, res) {
-  counter++;
-  return res.json({name: 'sunshine', counter: counter});
-});
+  counter++
+  return res.json({ name: 'sunshine', counter: counter })
+})
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080)
--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React, { useState, useEffect }  from 'react';
-import logo from './logo.png';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import logo from './logo.png'
+import './App.css'
 
-function App() {
-  const [data, setData] = useState({ name: 'unknown', counter: 0 });
+function App () {
+  const [data, setData] = useState({ name: 'unknown', counter: 0 })
 
   const fetchData = async () => {
-    const response = await fetch('/api/data');
-    const result = await response.json();
-    setData(result);
-  };
+    const response = await fetch('/api/data')
+    const result = await response.json()
+    setData(result)
+  }
 
-  useEffect (() => {
-    fetchData();
-  }, []);
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
-    <div className="main">
-      <img src={logo} alt="" width="100" /><br />
+    <div className='main'>
+      <img src={logo} alt='' width='100' /><br />
       Hello {data.name}, you called the backend {data.counter} times.
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
